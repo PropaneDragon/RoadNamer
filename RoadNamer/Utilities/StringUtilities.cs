a2e33285ad63286bbee207b8be0a44e09fc2fd81
Sean <thingshappen.xuras@gmail.com>
2015-11-21 01:18:54 +0000

    Label colouring!

---
+++ RoadNamer/Utilities/StringUtilities.cs
@@ -0,0 +1,29 @@
+﻿using ColossalFramework.UI;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+namespace RoadNamer.Utilities
+{
+    public static class StringUtilities
+    {
+        public static Color ExtractColourFromTags(string text, Color defaultColour)
+        {
+            Regex colourExtraction = new Regex("(?:<color)(#[0-9a-fA-F]+?)(>.*)");
+            string extractedTag = colourExtraction.Replace(text, "$1");
+
+            if(extractedTag != null && extractedTag != text && extractedTag != "")
+            {
+                defaultColour = UIMarkupStyle.ParseColor(extractedTag, defaultColour);
+            }
+
+            return defaultColour;
+        }
+
+        public static string RemoveTags(string text)
+        {
+            Regex tagRemover = new Regex("(<\\/?color.*?>)");
+                        
+            return tagRemover.Replace(text, "");
+        }
+    }
+}
---
+++ RoadNamerTests/StringUtilsTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RoadNamer.Utilities;
+using UnityEngine;
+
+namespace RoadNamerTests
+{
+    [TestClass]
+    public class StringUtilsTests
+    {
+        [TestMethod]
+        public void TestColourExtraction()
+        {
+            Color extractedColour = StringUtilities.ExtractColourFromTags("<color#ff0000>Red!</color>", new Color(1, 1, 1));
+            
+            Assert.IsTrue(extractedColour.r - 1.0f < 0.1); //Comparing floats is not fantastic
+            Assert.IsTrue(extractedColour.g - 0.0f < 0.1);
+            Assert.IsTrue(extractedColour.b - 0.0f < 0.1);
+
+            extractedColour = StringUtilities.ExtractColourFromTags("<color#00ff00>Green!</color>", new Color(1, 1, 1));
+
+            Assert.IsTrue(extractedColour.r - 0.0f < 0.1);
+            Assert.IsTrue(extractedColour.g - 1.0f < 0.1);
+            Assert.IsTrue(extractedColour.b - 0.0f < 0.1);
+
+            extractedColour = StringUtilities.ExtractColourFromTags("<color#0000ff>Blue!</color>", new Color(1, 1, 1));
+
+            Assert.IsTrue(extractedColour.r - 0.0f < 0.1);
+            Assert.IsTrue(extractedColour.g - 0.0f < 0.1);
+            Assert.IsTrue(extractedColour.b - 1.0f < 0.1);
+        }
+
+        [TestMethod]
+        public void TestTagRemoval()
+        {
+            string resultString = StringUtilities.RemoveTags("<color#ff0000>Red!</color>");
+            Assert.AreEqual<string>("Red!", resultString);
+
+            resultString = StringUtilities.RemoveTags("<color#ff0000><<<<--- ARROWS! --->>>></color>");
+            Assert.AreEqual<string>("<<<<--- ARROWS! --->>>>", resultString);
+        }
+    }
+}
